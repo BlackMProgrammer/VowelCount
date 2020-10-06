e50ae6c9f37ec370d1a626401e1e1fea7685fdda
BlackMProgrammer <cristhiancastro01@gmail.com>
2020-10-06 14:51:56 -0400

    Add project files.

---
+++ VowelCount/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace VowelCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetVowelCount("Cocotipulapia");
+        }
+
+        public static int GetVowelCount(string str) {
+
+            var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+
+            var readVowels = str.ToLower();
+
+            int vowelcount = readVowels.Count(c => vowels.Contains(c));
+            
+            Console.WriteLine(vowelcount);
+            return vowelcount;
+        }
+    }
+}
